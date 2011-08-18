6c1129cb34f28e7e16ec770df6538881203c0902
Gabriel Lesperance <gabriel@wavo.me>
2011-08-18 14:41:37 -0400

    RequireJS optimization now works properly !

--- spec/lib/test_project/client/js/root_client.js
+++ spec/lib/test_project/client/js/root_client.js
@@ -1,6 +1,40 @@
+
+
+(function () {
+var jq = typeof jQuery !== "undefined" && jQuery;
+define("jquery", [], function () { return jq; });
+}());
+
+
+
+(function () {
+var jq = typeof jQuery !== "undefined" && jQuery;
+define("jquery", [], function () { return jq; });
+}());
+
+
+
+(function () {
+var jq = typeof jQuery !== "undefined" && jQuery;
+define("jquery", [], function () { return jq; });
+}());
+
+$.fn.alpha = function() {
+    return this.append('<p>Alpha is Go!</p>');
+};
+define("lib/jquery.alpha", function(){});
+
+$.fn.beta = function() {
+    return this.append('<p>Beta is Go!</p>');
+};
+
+define("lib/jquery.beta", function(){});
+
 require(["jquery", "lib/jquery.alpha", "lib/jquery.beta"], function($) {
     //the jquery.alpha.js and jquery.beta.js plugins have been loaded.
     $(function() {
         $('body').alpha().beta();
     });
 });
+
+define("root_client", function(){});
